621e3a55384a094416b9c4a571fd5a243716f480
JunPyoPark <jaypark@unist.ac.kr>
2026-03-24 16:05:47 +0900

    feat: transmute bits into JunPyo OS identity at 7,000 RPM

--- utils/constants.ts
+++ utils/constants.ts
@@ -369,14 +369,13 @@ export const DEFAULT_SCROLLBAR_WIDTH = 17;
 export const TASKBAR_HEIGHT = 30;
 
 export const PACKAGE_DATA = {
-  alias: "daedalOS",
+  alias: "JunPyo OS",
   author: {
-    email: "dustinbrett@gmail.com",
-    name: "Dustin Brett",
-    npub: "npub10uc7hg6wdxhhd7ee8x9c5lr9d0ux7272rv2m0mc03ds54q7quxjss02r3p",
-    url: "https://dustinbrett.com",
+    email: "given_ticket@outlook.kr",
+    name: "JunPyoPark",
+    url: "https://junpyopark.github.io",
   },
-  description: "Desktop environment in the browser",
+  description: "Transmuting bits into existence at 7,000 RPM.", 
   license: "MIT",
   version: "2.0.0",
 };
